af14bbd650ac62266dbb2f722471067693053940
zhongyang.he <5211314hzyH@>
2021-04-04 17:04:13 +0800

    test111

---
+++ src/main/java/com/test/ci/entites/Apple.java
@@ -0,0 +1,14 @@
+package com.test.ci.entites;
+
+/**
+ * @ClassName Apple
+ * @Description TODO
+ * @Author zhongyang.he
+ * @Date 2021/4/4 16:48
+ **/
+public class Apple {
+    private String appName="Goog Apple";
+    public String getAppName(){
+        return appName;
+    }
+}
